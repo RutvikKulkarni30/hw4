b859dd1fcd3cd043faeccc379fc692da592bbfe1
RutvikKulkarni30 <rutvik.kulkarni@kellogg.northwestern.edu>
2026-03-17 18:01:44 -0500

    Update entries_controller.rb

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,18 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    if @current_user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = @current_user["id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    else
+      redirect_to "/sessions/new"
+    end
   end
 
 end
